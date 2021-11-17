bd5b255074ded4bc19a35599bc812266000fe2ca
uvanov <kirill.ivanov000@gmail.com>
2021-11-17 15:02:08 +0300

    Init files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const source = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    entry: path.resolve(source, 'js', 'index.js'),
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: "body",
+            filename: path.resolve(__dirname, 'index.html'),
+            template: path.resolve(source, 'html', 'index.html'),
+            minify: false
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    output: {
+        filename: "app.bundle.js",
+        path: path.resolve(dist, 'js'),
+        clean: true
+    }
+}
